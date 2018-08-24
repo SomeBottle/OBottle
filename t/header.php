193bbebc0a75fca011ae9916789dfea4848a99a4
SomeBottle <583797659@qq.com>
2018-08-24 12:44:21 +0800

    🌱更改文章页面样式

--- t/header.php
+++ t/header.php
@@ -1,4 +1,4 @@
-<link href="[css:style.css]" rel="stylesheet">
+<link href="[css:style.css?233]" rel="stylesheet">
 <script src='[js:m.js]'></script>
 <title>[name]</title>
 <div class='h'><h2><img src='[avatar]' style='border-radius:12px;max-width:40px;'></img><a href='#m' target='_self' style="text-decoration:none;color:black;">[name]</a></h2><p style='color:#AAA;font-size:15px;'>[intro]</p></div>
