381c215da7df5ec8785914e4b67e58b9c592d631
SomeBottle <583797659@qq.com>
2018-08-24 12:59:34 +0800

    🌹指定在新页面打开链接

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <base target="_blank">
       <meta name="viewport" content="width=device-width, initial-scale=1">
 	  <meta name="description" content="<?php echo descript();?>" />
 	  <meta name="keywords" content="<?php echo keyword();?>" />
