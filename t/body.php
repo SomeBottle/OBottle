4b0148769920c29c8a1a18789526beed08f948ae
SomeBottle <583797659@qq.com>
2019-02-16 14:03:29 +0800

    🍪 整合container到模板的body.php

---
+++ t/body.php
@@ -0,0 +1,8 @@
+<body>
+<div class='container'>
+<div id='h' class='header'></div>
+<div id='l'><img src='./c/load.png' style='max-width:60px;'></img></div>
+<div id='c'></div>
+<div id='f'></div>
+</div>
+</body>
