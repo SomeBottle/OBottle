d9157fb2588f1bdfdf572aa497e5427c08c4965c
SomeBottle <583797659@qq.com>
2019-02-17 10:48:37 +0800

    🍞 Tag Style Fix

--- t/header.php
+++ t/header.php
@@ -1,4 +1,4 @@
-<link href="[css:style.css?233]" rel="stylesheet">
+<link href="[css:style.css?[rand:1-233]]" rel="stylesheet">
 <!--示例模板<script src='[js:m.js]'></script>-->
 <title>[name]</title>
 <div class='h'><h2><img src='[avatar]' style='border-radius:12px;max-width:40px;'></img><a href='#m' target='_self' style="text-decoration:none;color:black;">[name]</a></h2><p style='color:#AAA;font-size:15px;'>[intro]</p></div>
